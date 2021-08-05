fdbb9d071f06374683032aa0f9e9c5a884a6ecd4
Jason Dsouza <jasmcaus@gmail.com>
2021-08-05 15:22:22 +0530

    Changes

--- cstl/buffer.h
+++ cstl/buffer.h
@@ -17,34 +17,32 @@ Copyright (c) 2021 Jason Dsouza <@jasmcaus>
 #include <string.h>
 #include <cstl/debug.h>
 #include <cstl/types.h>
+#include <cstl/string.h>
 
 typedef struct cstlBuffer cstlBuffer;
 
 struct cstlBuffer {
     char* data;    // buffer data
-    UInt64 length; // buffer size
+    UInt64 len;    // buffer size
 };
 
-// Create a new `cstlBuffer`
+
 static cstlBuffer* buff_new(char* buff_data);
-// Return the n'th character in the buffer data
 static char buff_at(cstlBuffer* buffer, UInt64 n);
-// Return a pointer to the beginning of the buffer data
 static char* buff_begin(cstlBuffer* buffer);
-// Return a pointer to the end of the buffer data
 static char* buff_end(cstlBuffer* buffer);
-// Is the buffer data empty?
 static bool buff_is_empty(cstlBuffer* buffer);
-// Append `buff2` to the buffer data
 static void buff_append(cstlBuffer* buffer, cstlBuffer* buff2);
-// Assign `new_buff` to the buffer data
 static void buff_set(cstlBuffer* buffer, char* new_buff);
-// Reset a buffer
+static UInt32 buff_len(cstlBuffer* buffer);
 static void buff_reset(cstlBuffer* buffer);
-// Reverse a buffer (non-destructive)
 static cstlBuffer* buff_rev(cstlBuffer* buffer);
-// Free the buffer from its associated memory
+static bool buff_cmp(cstlBuffer* buff1, cstlBuffer* buff2);
+static bool buff_cmp_nocase(cstlBuffer* buff1, cstlBuffer* buff2);
+static cstlBuffer* buff_slice(cstlBuffer* buffer, int begin, int bytes);
 static void buff_free(cstlBuffer* buffer);
+static inline cstlBuffer* buff_toupper(cstlBuffer* buffer);
+static inline cstlBuffer* buff_tolower(cstlBuffer* buffer);
 
 // Create a new `cstlBuffer`
 static cstlBuffer* buff_new(char* buff_data) {
@@ -61,13 +59,13 @@ static char buff_at(cstlBuffer* buffer, UInt64 n) {
     CSTL_CHECK_NOT_NULL(buffer, "Expected not null");
     CSTL_CHECK_NOT_NULL(buffer->data, "Expected not null");
     
-    if(n >= buffer->length)
+    if(n >= buffer->len)
         return nullchar;
 
     return (char)buffer->data[n];
 }
 
-// Return a pointer to the beginning of the buffer data
+// Returns a pointer to the beginning of the buffer data
 static char* buff_begin(cstlBuffer* buffer) {
     CSTL_CHECK_NOT_NULL(buffer, "Expected not null");
     CSTL_CHECK_NOT_NULL(buffer->data, "Expected not null");
@@ -75,12 +73,12 @@ static char* buff_begin(cstlBuffer* buffer) {
     return (char*)buffer->data;
 }
 
-// Return a pointer to the end of the buffer data
+// Returns a pointer to the end of the buffer data
 static char* buff_end(cstlBuffer* buffer) {
     CSTL_CHECK_NOT_NULL(buffer, "Expected not null");
     CSTL_CHECK_NOT_NULL(buffer->data, "Expected not null");
 
-    return (char*)(buffer->data + ((buffer->length - 1) * sizeof(char)));
+    return (char*)(buffer->data + ((buffer->len - 1) * sizeof(char)));
 }
 
 // Is the buffer data empty?
@@ -88,7 +86,77 @@ static bool buff_is_empty(cstlBuffer* buffer) {
     CSTL_CHECK_NOT_NULL(buffer, "Expected not null");
     CSTL_CHECK_NOT_NULL(buffer->data, "Expected not null");
 
-    return buffer->length == 0;
+    return buffer->len == 0;
+}
+
+// Returns the length of the buffer
+// Note: The implementation for this was taken from:
+// https://github.com/lattera/glibc/blob/master/string/strlen.c
+// All due credit for this goes to the rightful author.
+static UInt32 __internal_strlength(const char* str) {
+    const char* char_ptr;
+    const unsigned long int* longword_ptr;
+    unsigned long int longword, himagic, lomagic;
+
+    // Handle the first few characters by reading one character at a time.
+    // Do this until CHAR_PTR is aligned on a longword boundary.
+    for (char_ptr = str; ((unsigned long int) char_ptr & (sizeof (longword) - 1)) != 0; ++char_ptr) {
+        if (*char_ptr == nullchar)
+            return char_ptr - str;
+    }
+
+    // All these elucidatory comments refer to 4-byte longwords, but the theory applies equally 
+    // well to 8-byte longwords
+    longword_ptr = (unsigned long int* ) char_ptr;
+
+    // Bits 31, 24, 16, and 8 of this number are zero. Call these bits the "holes".
+    // Note that there is a hole just to the left of each byte, with an extra at the end:
+    //     bits:  01111110 11111110 11111110 11111111
+    //     bytes: AAAAAAAA BBBBBBBB CCCCCCCC DDDDDDDD
+    // The 1-bits make sure that carries propagate to the next 0-bit.
+    // The 0-bits provide holes for carries to fall into.
+    himagic = 0x80808080L;
+    lomagic = 0x01010101L;
+
+    if(sizeof(longword) > 4) {
+        // 64-bit version of the magic.
+        // Do the shift in two steps to avoid a warning if long has 32 bits.
+        himagic = ((himagic << 16) << 16) | himagic;
+        lomagic = ((lomagic << 16) << 16) | lomagic;
+    }
+    if(sizeof(longword) > 8)
+        exit(1);
+
+    // Instead of the traditional loop which tests each character, we will test a longword at a time.
+    // The tricky part is testing if *any of the four* bytes in the longword in question are zero.
+    for (;;) {
+        longword = *longword_ptr++;
+
+        if (((longword - lomagic) & ~longword & himagic) != 0) {
+            // Which of the bytes was the zero?  If none of them were, it was a misfire; continue the search.
+            const char* cp = (const char* ) (longword_ptr - 1);
+
+            if (cp[0] == 0)
+                return cp - str;
+            if (cp[1] == 0)
+                return cp - str + 1;
+            if (cp[2] == 0)
+                return cp - str + 2;
+            if (cp[3] == 0)
+                return cp - str + 3;
+
+            if (sizeof (longword) > 4) {
+                if (cp[4] == 0)
+                    return cp - str + 4;
+                if (cp[5] == 0)
+                    return cp - str + 5;
+                if (cp[6] == 0)
+                    return cp - str + 6;
+                if (cp[7] == 0)
+                    return cp - str + 7;
+            }
+        }
+    }
 }
 
 // Append `buff2` to the buffer data
@@ -99,7 +167,7 @@ static void buff_append(cstlBuffer* buffer, cstlBuffer* buff2) {
     CSTL_CHECK_NOT_NULL(buff2, "Expected not null");
     CSTL_CHECK_NOT_NULL(buff2->data, "Expected not null");
 
-    UInt64 new_len = buffer->length + buff2->length + 1;
+    UInt64 new_len = buffer->len + buff2->len + 1;
     char* newstr = (char*)calloc(1, new_len);
     strcpy(newstr, buffer->data);
     strcat(newstr, buff2->data);
@@ -111,13 +179,13 @@ static void buff_append_char(cstlBuffer* buffer, char ch) {
     CSTL_CHECK_NOT_NULL(buffer, "Expected not null");
     CSTL_CHECK_NOT_NULL(buffer->data, "Expected not null");
 
-    UInt64 len = buffer->length;
+    UInt64 len = buffer->len;
     char* newstr = (char*)calloc(1, len + 1);
     strcpy(newstr, buffer->data);
     newstr[len] = ch;
     newstr[len + 1] = nullchar;
     buff_set(buffer, newstr);
-    buffer->length += 2;
+    buffer->len += 2;
 }
 
 // Assign `new_buff` to the buffer data
@@ -129,11 +197,16 @@ static void buff_set(cstlBuffer* buffer, char* new_buff) {
         len = 0;
         new_buff = "";
     } else {
-        len = (UInt64)strlen(new_buff);
+        len = (UInt64)__internal_strlength(new_buff);
     }
 
     buffer->data = new_buff;
-    buffer->length = len;
+    buffer->len = len;
+}
+
+// Returns the buffer length
+static UInt32 buff_len(cstlBuffer* buffer) {
+    return buffer->len;
 }
 
 // Free the buffer from its associated memory
@@ -142,13 +215,13 @@ static void buff_reset(cstlBuffer* buffer) {
         return;
 
     buffer->data = null;
-    buffer->length = 0;
+    buffer->len = 0;
 }
 
 // Reverse a buffer (non-destructive)
 static Buff* buff_rev(Buff* buffer) {
     Buff* rev = buff_new(null);
-    UInt64 length = buffer->length;
+    UInt64 length = buffer->len;
     if(!length)
         return rev;
     
@@ -160,9 +233,106 @@ static Buff* buff_rev(Buff* buffer) {
     return rev;
 }
 
+// Compare two buffers (case-sensitive)
+// Returns true if `buff1` is lexicographically equal to `buff2`
+static bool buff_cmp(cstlBuffer* buff1, cstlBuffer* buff2) {
+    if(buff1->len != buff2->len)
+        return false;
+    
+    const unsigned char* s1 = (const unsigned char*) buff1->data;
+    const unsigned char* s2 = (const unsigned char*) buff2->data;
+    unsigned char ch1, ch2;
+
+    do {
+        ch1 = (unsigned char) *s1++;
+        ch2 = (unsigned char) *s2++;
+        if(ch1 == nullchar)
+            return (ch1 - ch2) == 0 ? true : false;
+    } while(ch1 == ch2);
+
+    return (ch1 - ch2) == 0 ? true : false;
+}
+
+// Compare two buffers (ignoring case)
+// Returns true if `buff1` is lexicographically equal to `buff2`
+static bool buff_cmp_nocase(cstlBuffer* buff1, cstlBuffer* buff2) {
+    if(buff1->len != buff2->len)
+        return false;
+    
+    const unsigned char* s1 = (const unsigned char*) buff1->data;
+    const unsigned char* s2 = (const unsigned char*) buff2->data;
+    unsigned char ch1, ch2;
+    int result;
+
+    if(s1 == s2)
+        return true;
+    
+    while((result = toLower(*s1) - toLower(*s2++)) == 0) {
+        if(*s1++ == nullchar)
+            break;
+    }
+    return result == 0 ? true : false;
+}
+
+// Get a slice of a buffer
+static cstlBuffer* buff_slice(cstlBuffer* buffer, int begin, int bytes) {
+    CSTL_CHECK_NOT_NULL(buffer, "`buffer` cannot be null");
+    CSTL_CHECK_GE(begin, 0);
+    CSTL_CHECK_GT(bytes, 0);
+
+    cstlBuffer* slice = buff_new(null);
+    CSTL_CHECK_NOT_NULL(slice, "`slice` cannot be null");
+    char* temp = (char*)calloc(1, bytes);
+    strncpy(temp, &(buffer->data[begin]), bytes);
+    buff_set(slice, temp);
+    CSTL_CHECK_NOT_NULL(slice, "`slice source` cannot be null");
+    return slice;
+}
+
 // Free the buffer from its associated memory
 static void buff_free(cstlBuffer* buffer) {
-    free(buffer);
+    if(buffer)
+        free(buffer);
+}
+
+// Convert a buffer to lowercase
+static inline cstlBuffer* buff_tolower(cstlBuffer* buffer) {
+    Buff* lower = buff_new(null);
+    if(!buffer->data) 
+        return lower;
+
+    char* temp = (char*)calloc(1, buffer->len);    
+    strcpy(temp, buffer->data);
+    int i = 0;
+    while(*temp) {
+        *temp = toLower(*temp);
+        temp++;
+        i++;
+    }
+    // Backtrack `i` times
+    temp -= i;
+    buff_set(lower, temp);
+    return lower;
+}
+
+// Convert a buffer to uppercase
+static inline cstlBuffer* buff_toupper(cstlBuffer* buffer) {
+    Buff* upper = buff_new(null);
+    if(!buffer->data) 
+        return upper;
+
+    char* temp = (char*)calloc(1, buffer->len);    
+    strcpy(temp, buffer->data);
+    int i = 0;
+    while(*temp) {
+        *temp = toUpper(*temp);
+        temp++;
+        i++;
+    }
+    // Backtrack `i` times
+    temp -= i;
+    buff_set(upper, temp);
+    return upper;
 }
 
 #endif // CSTL_BUFFER_H
--- cstl/string.h
+++ cstl/string.h
@@ -17,13 +17,11 @@ Copyright (c) 2021 Jason Dsouza <@jasmcaus>
 #include <cstl/types.h>
 #include <cstl/math.h>
 #include <cstl/debug.h>
-#include <stdlib.h> // for exit(1)
 
 // UTF8 Inspiration: https://github.com/sheredom/utf8.h/blob/master/utf8.h
 
-// Char Things ==========================================
-static inline bool isUpper(char c) { return c>='A' && c<='Z'; }
-static inline bool isLower(char c) { return c>='a' && c<='z'; }
+static inline bool isUpper(char c) { return c >= 'A' && c <= 'Z'; }
+static inline bool isLower(char c) { return c >= 'a' && c <= 'z'; }
 static inline bool isDigit(char c) { return c >= '0' && c <= '9'; }
 static inline bool isAlpha(char c) { return isUpper(c) || isLower(c); }
 static inline bool isAlphanumeric(char c) { return isAlpha(c) || isDigit(c); }
@@ -41,7 +39,6 @@ static inline bool isHexDigit(char c) {
            CSTL_IS_BETWEEN(c, 'a', 'f') ||
            CSTL_IS_BETWEEN(c, 'A', 'F'); 
 }
-
 
 static inline bool isLetter(char c) {
     return  (c >= 'a' && c <= 'z') || 
@@ -67,7 +64,9 @@ static inline bool isWhitespace(char c) {
     return false;
 }
 
-static inline Int32 digitToInt(char c) { return isDigit(c) ? c-'0' : c-'W'; }
+static inline Int32 digitToInt(char c) { 
+    return isDigit(c) ? c - '0' : c - 'W'; 
+}
 
 static inline Int32 hexDigitToInt(char c) {
     if(isDigit(c))
@@ -80,30 +79,6 @@ static inline Int32 hexDigitToInt(char c) {
         return c-'A' + 10; 
 
     return -1; 
-}
-
-static inline void strToLower(char* str) {
-    if(!str) return; 
-    while(*str) {
-        *str = toLower(*str);
-        str++;
-    }
-}
-
-static inline void strToUpper(char* str) {
-    if(!str) return; 
-    while(*str) {
-        *str = toUpper(*str);
-        str++;
-    }
-}
-
-// Get a substring from `source` and copies it into `destination`
-static inline void substr(char* destination, char const* source, int begin, int bytes) {
-    CSTL_CHECK_NOT_NULL(destination, "`destination` cannot be null. Did you forget to allocate memory for it?");
-    CSTL_CHECK_NOT_NULL(source, "`source` cannot be null");
-    CSTL_CHECK_GE(begin, 0);
-    strncpy(destination, &(source[begin]), bytes);
 }
 
 #endif // CSTL_STRING_H
