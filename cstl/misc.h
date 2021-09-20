339e3aaab960b3a2d05c22659b5275e2f20e99c2
Jason Dsouza <jasmcaus@gmail.com>
2021-09-20 19:38:43 +0530

    Update core

--- cstl/misc.h
+++ cstl/misc.h
@@ -138,6 +138,12 @@ Copyright (c) 2021 Jason Dsouza <@jasmcaus>
 #ifndef CORETEN_STATIC
     #define CORETEN_STATIC       static
 #endif
+AND_OR_TYPES
+#ifndef AND_OR_TYPES
+    #define AND_OR_TYPES
+    #define and     &&
+    #define or      ||
+#endif // AND_OR_TYPES
 
 #ifndef CORETEN_GLOBAL
     #define CORETEN_GLOBAL       static // Global Variables
